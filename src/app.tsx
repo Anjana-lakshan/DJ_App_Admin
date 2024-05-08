b46b48be18ceb1471e01f0818f7fce1a6325e491
Anjana Lakshan <anjana@konekt.lk>
2024-05-08 20:18:38 +0530

    feat: Update app

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,10 @@
-import { Footer, Question, SelectLang, AvatarDropdown, AvatarName } from '@/components';
-import { LinkOutlined } from '@ant-design/icons';
+// import { AvatarDropdown, AvatarName } from '@/components';
+import { currentUser as queryCurrentUser } from '@/services/ant-design-pro/api';
 import type { Settings as LayoutSettings } from '@ant-design/pro-components';
-import { SettingDrawer } from '@ant-design/pro-components';
 import type { RunTimeLayoutConfig } from '@umijs/max';
-import { FormattedMessage, history, Link } from '@umijs/max';
+import { history } from '@umijs/max';
 import defaultSettings from '../config/defaultSettings';
 import { errorConfig } from './requestErrorConfig';
-import { currentUser as queryCurrentUser } from '@/services/ant-design-pro/api';
-import React from 'react';
 const isDev = process.env.NODE_ENV === 'development';
 const loginPath = '/user/login';
 
@@ -54,10 +51,10 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
     // actionsRender: () => [<Question key="doc" />, <SelectLang key="SelectLang" />],
     avatarProps: {
       src: initialState?.currentUser?.avatar,
-      title: <AvatarName />,
-      render: (_, avatarChildren) => {
-        return <AvatarDropdown>{avatarChildren}</AvatarDropdown>;
-      },
+      // title: <AvatarName />,
+      // render: (_, avatarChildren) => {
+      //   return <AvatarDropdown>{avatarChildren}</AvatarDropdown>;
+      // },
     },
     waterMarkProps: {
       // content: initialState?.currentUser?.name,
